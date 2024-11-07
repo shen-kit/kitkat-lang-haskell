8678642cee0ad480a008b13ff48da0f29609dabb
Shen-Kit Hia <shenkit108+1@gmail.com>
2024-11-07 16:57:13 +1100

    adds more tests to TestCodegen

--- haskell_spike/tests/TestCodegen.hs
+++ haskell_spike/tests/TestCodegen.hs
@@ -3,11 +3,12 @@
 
 module Main where
 
-import Parser.ParserTypes (Type (..))
+import Parser.ParserTypes (Type (..), BOp (..))
 import Parser.SemantParserTypes
 import Midend.CodegenInner
 
 import Control.Monad.State (evalState)
+import LLVM.AST.IntegerPredicate as IP
 import LLVM.IRBuilder qualified as IR
 import LLVM.AST qualified as AST
 import Test.HUnit
@@ -15,21 +16,30 @@ import Data.Map qualified as M
 
 -- HELPERS
 
-initEnv = Env { strings = M.empty, operands = M.empty }
+initialEnv = Env { strings = M.empty, operands = M.empty }
 
 evalCodegenSexpr :: SExpr -> AST.Operand
 evalCodegenSexpr sexpr =
-  fst $ fst $ evalState (IR.runModuleBuilderT IR.emptyModuleBuilder (IR.runIRBuilderT IR.emptyIRBuilder (codegenSexpr sexpr))) initEnv
+  fst $ fst $ evalState (IR.runModuleBuilderT IR.emptyModuleBuilder (IR.runIRBuilderT IR.emptyIRBuilder (codegenSexpr sexpr))) initialEnv
 
 -- TESTS
 
-tests :: Test
-tests = TestList [
-    "int literal 100" ~: do
-      let sexpr = (TyInt, SInt 100)
-      let expected = IR.int32 100
-      evalCodegenSexpr sexpr @?= expected
+-- test Operand result of code-generation from semantic expression
+testCodegenSexpr :: Test
+testCodegenSexpr = TestList [
+    -- integers
+    "int literal min" ~: evalCodegenSexpr (TyInt, SInt $ -2147483648) @?= IR.int32 (-2147483648),
+    "int literal 0" ~: evalCodegenSexpr (TyInt, SInt 0) @?= IR.int32 0,
+    "int literal 100" ~: evalCodegenSexpr (TyInt, SInt 100) @?= IR.int32 100,
+    "int literal max" ~: evalCodegenSexpr (TyInt, SInt 2147483647) @?= IR.int32 2147483647,
+
+    -- boolean
+    "bool literal false" ~: evalCodegenSexpr (TyBool, SBool False) @?= IR.bit 0,
+    "bool literal true" ~: evalCodegenSexpr (TyBool, SBool True) @?= IR.bit 1
   ]
+
+tests :: Test
+tests = TestList [testCodegenSexpr]
 
 main :: IO ()
 main = runTestTTAndExit tests
